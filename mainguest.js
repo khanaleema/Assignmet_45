cf146bcc190d17f94e47ce226d221207dfe333ed
Aleema Khan <161638702+khanaleema@users.noreply.github.com>
2024-02-28 12:54:07 -0800

    EX:14

--- mainguest.js
+++ mainguest.js
@@ -1,4 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.guest_list = void 0;
 var guest_list = ['aain', 'aalam', 'hanzala', 'hoor', 'aleema'];
+exports.guest_list = guest_list;
 for (var i = 0; i < guest_list.length; i++) {
     console.log('Respected sir/Madam' + guest_list[i] + ',\nWe invited you on dinner tomorrow.\n\nThank You');
 }
---
+++ mainguest.ts
@@ -0,0 +1,5 @@
+let guest_list : string[] =['aain', 'aalam', 'hanzala', 'hoor', 'aleema'];
+for(let i=0; i<guest_list.length; i++) {
+     console.log( 'Respected sir/Madam' + guest_list[i] + ',\nWe invited you on dinner tomorrow.\n\nThank You')
+}
+export{guest_list}
