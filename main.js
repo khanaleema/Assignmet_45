78775f1820c32388122501ff1331cd59377fd555
Aleema Khan <161638702+khanaleema@users.noreply.github.com>
2024-02-28 12:34:05 -0800

    EX:2

---
+++ main.js
@@ -0,0 +1,2 @@
+var personName = "aleemakhan";
+console.log("Hellow ".concat(personName, "would you like to learn some basic language today?"));
---
+++ main.ts
@@ -0,0 +1,2 @@
+let personName: string= "aleemakhan"
+console.log (`Hellow ${personName}would you like to learn some basic language today?`);
