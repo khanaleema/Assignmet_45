99d68256076d68d97848e5f76214c325ef2c285d
Aleema Khan <161638702+khanaleema@users.noreply.github.com>
2024-02-28 13:20:03 -0800

    EX:42

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-function show_magicians(magicians) {
-    for (var _i = 0, magicians_1 = magicians; _i < magicians_1.length; _i++) {
-        var magician_1 = magicians_1[_i];
-        console.log(magician_1.charAt(0).toUpperCase() + magician_1.slice(1));
+function make_great(magicians) {
+    for (var i = 0; i < magicians.length; i++) {
+        magicians[i] = magicians[i] + ' the great';
+        console.log(magicians[i]);
     }
 }
-var magician = ["saeer aalam", "burhan dil sher", "hashim khan"];
-show_magicians(magician);
+var magicians = ["Harry Porter", "Ron Weasly", "David Copperfield"];
+make_great(magicians);
--- main.ts
+++ main.ts
@@ -1,8 +1,9 @@
-function show_magicians(magicians: string[]) : void {
-
-    for (const magician of magicians) {
-        console.log(magician.charAt(0).toUpperCase() + magician.slice(1));
+function make_great(magicians: string[]) : void {
+    for (let i = 0; i < magicians.length; i++) {
+        magicians[i] = magicians[i] +  ' the great'
+        console.log(magicians[i])
     }
 }
-const magician: string[] = ["saeer aalam" , "burhan dil sher" , "hashim khan"];
-show_magicians(magician)
+
+const magicians: string[] = ["Harry Porter", "Ron Weasly", "David Copperfield"];
+make_great(magicians)
