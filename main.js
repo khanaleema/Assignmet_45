e3f175b12c4c981ae0dfdaec17fb854611296697
Aleema Khan <161638702+khanaleema@users.noreply.github.com>
2024-02-28 12:57:38 -0800

    EX:15

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 var guest_list = ['aain', 'aalam', 'hanzala', 'hoor', 'aleema'];
 for (var i = 0; i < guest_list.length; i++) {
-    console.log('Respected sir/Madam' + guest_list[i] + ',\nWe invited you on dinner tomorrow.\n\nThank You');
+    console.log('Respected sir/Madam' + guest_list[i] + ',\nWe invited you on dinner tomorrow.\nThank You\n');
 }
+var not_present = 'hanzala';
+var new_guest = 'maleeka';
+for (var i = 0; i < guest_list.length; i++) {
+    console.log('Respected sir/Madam' + guest_list[i] + ',\nWe invited you on dinner tomorrow.\nThank You\n');
+}
+console.log("Mr. ".concat(not_present, " will not coming for tomorrow dinner."));
--- main.ts
+++ main.ts
@@ -1,4 +1,10 @@
-let transportation: string[] =['suzuki wagron R', 'civic', 'honda city', 'bike', 'toyota hilux'];
-for(let i = 0; i<transportation.length; i++) {
-     console.log( 'I would like to own' + transportation[i]);
+let guest_list : string[] =['aain', 'aalam', 'hanzala', 'hoor', 'aleema'];
+for(let i = 0; i<guest_list.length; i++) {
+     console.log( 'Respected sir/Madam' + guest_list[i] + ',\nWe invited you on dinner tomorrow.\nThank You\n')
 }
+let not_present : string = 'hanzala';
+let new_guest : string = 'maleeka';
+for(let i = 0; i<guest_list.length; i++) {   
+      console.log( 'Respected sir/Madam' + guest_list[i] + ',\nWe invited you on dinner tomorrow.\nThank You\n')
+}
+console.log(`Mr. ${not_present } will not coming for tomorrow dinner.`)
