ca1b00991bc43e6cf5f73a67023a1f13273ef3d2
Aleema Khan <161638702+khanaleema@users.noreply.github.com>
2024-02-28 12:48:51 -0800

    EX:10

---
+++ program1.js
@@ -0,0 +1,5 @@
+// exercise 10
+// my name is ALEEMA KHAN
+// Dated: 24/02/2024
+// this program will run simple code just like (question?)
+console.log("Hi can we learn some basic language today?");
