51fcddf4a317282427381631b8b3246aaed6273a
Aleema Khan <161638702+khanaleema@users.noreply.github.com>
2024-02-28 13:06:49 -0800

    EX:28

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
 "use strict";
-let alien_color = "red";
-if (alien_color == "green") {
-    console.log(" 5 points ");
+let age = 100;
+if (age < 2) {
+    console.log("you are a babay");
 }
-else if (alien_color == "yellow") {
-    console.log(" 10 points ");
+else if (age < 4) {
+    console.log("you are a toddler");
+}
+else if (age < 13) {
+    console.log("you are a kid");
+}
+else if (age < 20) {
+    console.log("you are a teenager");
+}
+else if (age < 65) {
+    console.log("you are a adult");
 }
 else {
-    console.log(" 15 points ");
+    console.log("you are older");
 }
--- main.ts
+++ main.ts
@@ -1,8 +1,15 @@
-let alien_color : string = "red"
-if(alien_color == "green") {
-    console.log( " 5 points ")
-} else if(alien_color == "yellow") {
-    console.log( " 10 points ")
+let age : number = 100
+
+if (age < 2) {
+    console.log("you are a babay")
+} else if (age < 4)  { 
+    console.log("you are a toddler")
+} else if (age < 13) {
+    console.log("you are a kid")
+} else if (age < 20) {
+    console.log("you are a teenager")
+} else if (age < 65) {
+    console.log("you are a adult")
 } else {
-    console.log( " 15 points ")
-}
+    console.log("you are older")
+} 
