ac6bca2d4991bf04798ab3881610f8c94bac4f96
Aleema Khan <161638702+khanaleema@users.noreply.github.com>
2024-02-28 13:12:35 -0800

    EX:31

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 var users = ["admin", "aleema", "saeer", "hoor"];
-for (var _i = 0, users_1 = users; _i < users_1.length; _i++) {
-    var user = users_1[_i];
-    if (user == "admin") {
-        console.log("Hello admin, would you like to see a status report?");
-    }
-    else {
-        console.log(" Hello ".concat(user, ", Thank you for logging in again"));
-    }
+// if (users.length === 0) {
+//     console.log("We need to find some users!")
+// } else {
+//     for(let user of users) {
+//         if (user === "admin") {
+//             console.log("Hello admin, would you like to see a status report?")
+//         } else {
+//             console.log(` Hello ${user}, Thank you for logging in again`)
+//         }
+//     }
+// }
+users = [];
+if (users.length === 0) {
+    console.log("We need to find some users!");
 }
--- main.ts
+++ main.ts
@@ -1,8 +1,18 @@
 let users : string [] = ["admin" , "aleema" , "saeer" , "hoor"]
-for(let user of users) {
-    if (user == "admin") {
-        console.log("Hello admin, would you like to see a status report?")
-    } else {
-        console.log(` Hello ${user}, Thank you for logging in again`)
-    }
+
+// if (users.length === 0) {
+//     console.log("We need to find some users!")
+// } else {
+//     for(let user of users) {
+//         if (user === "admin") {
+//             console.log("Hello admin, would you like to see a status report?")
+//         } else {
+//             console.log(` Hello ${user}, Thank you for logging in again`)
+//         }
+//     }
+// }
+
+users = []
+if (users.length === 0) {
+    console.log("We need to find some users!")
 }
