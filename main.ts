ac6d2e91f65c2aa41d43b482d197aeb4f2a136db
Aleema Khan <161638702+khanaleema@users.noreply.github.com>
2024-02-28 13:03:45 -0800

    EX:23

--- main.ts
+++ main.ts
@@ -1,2 +1,30 @@
-let array: (string | number) [] = ['aain' , 'aalam' ,3,5,8, "aleema"]
-console.log(array[5])
+let car = "honda";
+console.log("is car == 'honda'? predict true")
+console.log(car == 'honda')
+
+console.log("is car != 'suzki vegron r'? predict false")
+console.log(car == 'suzuki vegron r')
+
+console.log("is 3 <= 5 ? predict true")
+console.log( 3 <= 5)
+
+console.log("is car == 'SUZUKI VAGRON R'? predict false")
+console.log(car == 'SUZUKI VAGRON R')
+
+console.log("is car.lenght != 15 ? predict true")
+console.log(car.length != 15)
+
+console.log("is 10 <= 2? predict false")
+console.log( 10 <= 2)
+
+console.log("is 2 < 6? predict true")
+console.log(2 < 6 )
+
+console.log("is 8 <= 6? predict false")
+console.log(8 <= 6 )
+
+console.log("is 42 == 42 true")
+console.log(42 == 42)
+
+console.log("is 72 >= 83? predict false")
+console.log(72 >= 83 )
