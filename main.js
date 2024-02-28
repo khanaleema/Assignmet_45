e1d52d2d3c92d2ac255eba63e312d7baafe50c54
Aleema Khan <161638702+khanaleema@users.noreply.github.com>
2024-02-28 13:01:41 -0800

    Add files via upload
    EX:20

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var mainguest_js_1 = require("../Ex14 (Guest List)/mainguest.js");
-console.log("n PRINTING NUMBER OF GUEST INVITED");
-console.log("we had finally invited ".concat(mainguest_js_1.guest_list.length, " from exercise 14"));
+var languages = ['Urdu', 'Italian', 'Arabic', 'farsi', 'pashto'];
+console.log("list of languages:");
+for (var _i = 0, languages_1 = languages; _i < languages_1.length; _i++) {
+    var top_1 = languages_1[_i];
+    console.log(top_1);
+}
--- main.ts
+++ main.ts
@@ -1,3 +1,5 @@
-import{ guest_list } from '../Ex14 (Guest List)/mainguest.js'
-console.log(`n\ PRINTING NUMBER OF GUEST INVITED`)
-console.log(`we had finally invited ${guest_list.length} from exercise 14`)
+let languages : string [] = ['Urdu' , 'Italian' , 'Arabic' , 'farsi' , 'pashto']
+console.log("list of languages:")
+for(let top of languages){
+    console.log(top)
+} 
